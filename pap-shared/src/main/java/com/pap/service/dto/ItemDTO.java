016ac848d3ab82125819a257d95321ce447aa91d
datngo <dtuyt0@gmail.com>
2021-08-14 13:50:42 +0700

    PD12: them api cho admin

--- pap-shared/src/main/java/com/pap/service/dto/ItemDTO.java
+++ pap-shared/src/main/java/com/pap/service/dto/ItemDTO.java
@@ -1,11 +1,15 @@
 package com.pap.service.dto;
 
 import com.pap.domain.Item;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDTO {
 
     private String id;
